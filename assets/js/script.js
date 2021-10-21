1095bddabdcde4fdddb82ea0c70853418c161875
Tyreeze <wedding5@msu.edu>
2021-10-21 11:12:05 -0500

    add submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() { 
-  var listItemEl = document.createElement("li"); //create new task itemk item
+var createTaskHandler = function(event) { 
+    console.log(event);
+    event.preventDefault();
+
+  var listItemEl = document.createElement("li"); //create new task item
   listItemEl.className = "task-item";  // style the new task item
   listItemEl.textContent = "This is a new task.";  // add the text
   tasksToDoEl.appendChild(listItemEl);  // append the element to the task
   }; 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
